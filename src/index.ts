5a50e7279cc79f419e213d49aa0c3d124c4ce03f
Tim de Koning <git@king-management.nl>
2021-12-19 23:45:29 +0100

    Add server logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { program } from "commander";
 import * as chromecast from "./chromecast";
 import * as video from "./video";
+import * as server from "./server";
+import getPort from "get-port";
+
 program
   .version("0.0.1")
   .description("Play any video on your Chromecast")
@@ -9,13 +12,14 @@ program
   .argument("[path to subtitles file]", "Subtitles to show")
   .action(async (videoPath, subtitlesPath) => {
     const options = program.opts();
-    console.log(options);
-
     console.log(`Movie path: ${videoPath}`);
     console.log(`Subtitles path: ${subtitlesPath || "(none)"}`);
+    console.log(`Options: ${JSON.stringify(options)}`);
+
+    const port = await getPort();
+    await server.initialize(port);
     const myChromecast = await chromecast.initialize(options.chromecast);
-    console.log(myChromecast);
-    // video.initialize(videoPath);
+    video.initialize(videoPath);
   });
 
 program.parse(process.argv);
---
+++ src/server/index.ts
@@ -0,0 +1,52 @@
+import { createReadStream, statSync } from "fs";
+import express from "express";
+import cors from "cors";
+
+let videoPath = "";
+let subtitlePath = "";
+
+export async function initialize(port: number) {
+  // END OF CONFIG
+  const mediaServerApp = express();
+
+  mediaServerApp.use(cors());
+
+  mediaServerApp.get("/subtitles", function (req, res) {
+    res.setHeader("Content-Type", "text/vtt");
+    res.sendFile(subtitlePath);
+  });
+  mediaServerApp.get("/video", function (req, res) {
+    const { range = "" } = req.headers;
+    if (!range) {
+      res.status(400).send("Requires Range header");
+    }
+    const videoSize = statSync(videoPath).size;
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+    const videoStream = createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
+  });
+  return new Promise((resolve) => {
+    mediaServerApp.listen(port, () => {
+      console.log(`Mediaserver running on port ${port}`);
+      resolve(mediaServerApp);
+    });
+  });
+}
+
+export const setMediaServerVideoPath = (newVideoPath: string): void => {
+  videoPath = newVideoPath;
+};
+
+export const setMediaServerSubtitlesPath = (newSubtitlesPath: string): void => {
+  subtitlePath = newSubtitlesPath;
+};
